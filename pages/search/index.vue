bb735f073ce1a2a1dbb32077b92cbdef1722ff6a
Xaenerys <atipper@vtc.edu>
2019-09-24 18:11:11 -0400

    More updates

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="container">
         <h1>Search</h1>
-        <p><nuxt-link to="/">Home</nuxt-link></p>
+        <p>Enter a search query below using a text from a card name. A list will be populated based on the results.</p>
         <input v-model="cardSearch" type="text" name="search" @keyup.enter="saveCard(cardSearch)"/>
-        <button type="text" @click="saveCard(cardSearch)">Search</button>
+        <button type="text" @click="saveCard(cardSearch)" v-bind:disabled="cardSearch.length === 0">Search</button>
         <br>
-        <ul>
-            <li v-for="card in cards" :key="card.id">{{ card.name }}</li>
-        </ul>
+        <table v-if="cards.length >= 1" class="cardResults">
+            <tr v-for="card in cards" :key="card.id">
+                <td>{{ card.name }}</td>
+                <td>{{ card.setName }}</td>
+                <td>{{ card.imageURL }}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -17,16 +21,19 @@ export default {
     data() {
         return {
             cards: [],
-            cardSearch: null
+            cardSearch: ''
         }
     },
     methods: {
         saveCard: function(cardName) {
+            this.cards = []
             Magic.card.all({name: cardName, pageSize: 1})
             .on('data', card => {
                 this.cards.push({
                     name: card.name,
-                    id: card.id
+                    id: card.id,
+                    setName: card.setName,
+                    imageURL: card.imageURL
                 })
                 this.cardSearch = ''
             })
@@ -39,9 +46,20 @@ export default {
 input {
     background-color:lightslategray;
 }
+
 button {
     background-color:darkgray;
     border-radius: 25%;
     padding: 2px;
+}
+
+button:disabled {
+    background-color:gainsboro;
+    color: black;
+}
+
+table,tr,td {
+    border: rgb(255, 255, 255) !important;
+    padding: 3px;
 }
 </style>
