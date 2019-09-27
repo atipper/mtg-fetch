d7892089e92d8f1f6966808af187b277e1ef85b7
Andrew Tipper <andrewt@vermontsystems.com>
2019-09-27 17:14:23 -0400

    Firebase init

--- pages/sets/index.vue
+++ pages/sets/index.vue
@@ -13,10 +13,12 @@
 
 <script>
 import axios from 'axios'
+import database from '../../components/firebaseInit'
 export default {
     data() {
         return {
-            sets: [],
+            cards: null,
+            sets: null,
             info: null,
             filtered: null
         }
@@ -39,13 +41,35 @@ export default {
         assignSets: function() {
             this.sets = this.info.data.data
         },
-        log: function() {
-            console.log(this.scrySet[0])
-        }
-    },
-    computed: {
-        filteredSets() {
+        fetchCards: function() {
             
+        },
+        assignCards: function() {
+
+        },
+        storeSets: function() {
+            this.db.collection('mtgSets').add({
+                name: this.sets.name,
+                block: this.sets.block,
+                set_type: this.sets.set_type,
+                id: this.sets.id,
+                slug: this.generateUUID()
+            })
+            .then(function(docRef) {
+                console.log('Document written with ID: ', docRef.id)
+            })
+            .catch(function(error) {
+                console.error('Error adding document: ', error)
+            })
+        },
+        generateUUID() {
+            let d = new Date().getTime()
+            let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                let r = (d + Math.random() * 16) % 16 | 0
+                d = Math.floor(d / 16)
+                return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+            })
+            return uuid
         }
     }
 }
--- static/scripts.js
+++
@@ -1,7 +0,0 @@
-export function blackLotus() {
-    const mtg = require('mtgsdk');
-
-    mtg.card.find(3).then(result =>{
-        console.log(result.card.name)
-    })
-}
