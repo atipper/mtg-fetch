8465cfd9c6ec1c619986b84ad47353246d9d50b3
Andrew Tipper <andrewtipper17@gmail.com>
2022-04-12 09:53:18 -0400

    Update fetch project so it is in a working state

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,19 +1,22 @@
 // /plugins/firebase.js
-import firebase from 'firebase'
 
-let firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appID: process.env.FIREBASE_APP_ID,
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { database } from 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-let app = null
-if (!firebase.apps.length) {
-  app = firebase.initializeApp(firebaseConfig)
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appID: process.env.FIREBASE_APP_ID,
 }
 
-export const db = app.database()
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = database
